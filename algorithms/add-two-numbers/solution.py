d0aecb8c8824a70b8a9ab440039bbdc65c92b583
Ankit Kumar Singh <singhankit64566@gmail.com>
2026-08-20 14:06:30 +0530

    feat: add Add Two Numbers solution

--- algorithms/add-two-numbers/solution.py
+++ algorithms/add-two-numbers/solution.py
@@ -1,7 +1,41 @@
-class Solution(object):
-    def moveZeroes(self, nums):
-        insert_pos=0
-        for i in range(len(nums)):
-            if nums[i] !=0:
-                nums[insert_pos],nums[i]=nums[i],nums[insert_pos]
-                insert_pos +=1
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution(object):
+#     def addTwoNumbers(self, l1, l2):
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+
+            total = x + y + carry
+            carry = total // 10
+
+            curr.next = ListNode(total % 10)
+            curr = curr.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
